15f3be9717062e4d2bad4238419091a115d699d6
Ximb <100860445+Ximbeh@users.noreply.github.com>
2022-03-10 12:56:14 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,89 @@
+inteiro = 10
+real = 10.45
+cancelou = true
+
+//1
+cliente1 = "Von Mises"
+cliente2 = "Jhon Locke"
+cliente3 = "Adam Smith"
+
+//2
+comida1 = "Banana"
+comida2 = "Pepino"
+comida3 = "Berinjela"
+comida4 = "Mandioca"
+comida5 = "Cenoura"
+
+//3
+nome1 = "Francois"
+sobrenome1 = "Voltaire"
+nome2 = "Charles"
+sobrenome2 = "Montesquieu"
+
+//4
+nome3 = "Cairé"
+nome4 = "Bernardo"
+nome5 = "Venus"
+nome6 = "Jaiza"
+nome7 = "Enzo"
+nome8 = "Manu"
+
+Mitou = true
+Crinjou = true
+Tankou = false
+Redpillou = false
+Bluepillou = true
+Bostejou = true
+
+//5
+
+
+//6
+numero1 = "1"
+numero2 = "2"
+numero3 = "3"
+numero4 = "4"
+numero5 = "5"
+numero6 = "6"
+numero7 = "7"
+numero8 = "8"
+
+//7
+
+
+//8
+
+
+//9
+
+
+//10
+
+
+//11
+
+
+//12
+
+
+//13
+
+
+//14
+
+
+//15
+
+
+//consoles.logs
+console.log(inteiro, real, cancelou, cliente1, cliente2, cliente3, comida1, comida2, comida3, comida4, comida5, nome1 + sobrenome1, nome2 + sobrenome2)
+console.log (nome3, Mitou)
+console.log (nome4, Crinjou)
+console.log (nome5, Tankou)
+console.log (nome6, Redpillou)
+console.log (nome7, Bluepillou)
+console.log (nome8, Bostejou)
+console.log (numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8)
+
+
+//Lenght (PESQUISAR ISSO)
